37201d172867e7c8b23367856f991144880ec01d
unknown <agarwalmehul187@gmail.com>
2026-06-11 20:23:44 +0530

    Day-11 uploaded

---
+++ Day-11/Q43.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isprime(int n){
+    if(n<=1)
+    return false;
+    for(int i=2; i<=n/2; i++)
+    {if (n% i== 0)
+    return false;
+    }
+    return true;
+}
+int main(){
+    int num;
+    cout<< "Enter a number:" ;
+    cin>> num;
+
+    if(isprime(num))
+    cout<< "Prime" ;
+    else
+    cout<< "Not Prime" ;
+    return 0;
+}
